ceab512d659d4a92043724f39b2171bf86278913
Maxi <maximilian.lincks@gmx.net>
2022-05-13 10:50:20 +0200

    refactored loop with stream refactored Vector with generified List

--- main/src/Customer.java
+++ main/src/Customer.java
@@ -3,42 +3,47 @@ import java.lang.*;
 import java.util.*;
 
 class Customer {
-    private String name;
-    private Vector rentals = new Vector();
+    private final String name;
+
+    private final List<Rental> rentals = new ArrayList<>();
+
     public Customer (String newname){
         name = newname;
-    };
+    }
+
     public void addRental(Rental arg) {
-        rentals.addElement(arg);
-    };
+        rentals.add(arg);
+    }
     public String getName (){
         return name;
-    };
+    }
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
-        String result = "Rental Record for " + this.getName() + "\n";
-        result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = amountFor(each);
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == MovieType.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints ++;
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-        }
+        double totalAmount = rentals.stream().mapToDouble(this::amountFor).sum();
+
+        long frequentRenterPoints = rentals.size() +
+                // add bonus for a two day new release rental
+                rentals.stream()
+                        .filter(rental -> rental.getDaysRented() > 1)
+                        .filter(rental -> rental.getMovie().getPriceCode() == MovieType.NEW_RELEASE)
+                        .count();
+
+        StringBuilder result = new StringBuilder("Rental Record for " + this.getName() + "\n")
+                .append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
+
+        rentals.stream()
+                        .map(rental -> "\t" + rental.getMovie().getTitle()+ "\t" + "\t" + rental.getDaysRented() + "\t" + amountFor(rental) + "\n")
+                        .forEach(result::append);
+
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-        return result;
+        result.append("Amount owed is ").
+                append(totalAmount).
+                append("\n").
+                append("You earned ").
+                append(frequentRenterPoints).
+                append(" frequent renter points");
+
+        return result.toString();
     }
 
     private double amountFor(Rental each) {
